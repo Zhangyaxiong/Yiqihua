960ad4ea86055bb7821094adef7494aa52b67e93
Zhangyaxiong <792783226@qq.com>
2015-08-21 18:11:37 +0800

    添加下拉菜单

--- ArtExam/AE_draw_info_left.h
+++ ArtExam/AE_draw_info_left.h
@@ -1,13 +1,13 @@
 //
-//  AE—draw_info_left.h
+//  AE_draw_info_left.h
 //  ArtExam
 //
-//  Created by 张亚雄 on 15/8/19.
+//  Created by 张亚雄 on 15/8/20.
 //  Copyright (c) 2015年 张亚雄. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AE_draw_info_left : UITableViewCell
+@interface AE_draw_info_left : UIViewController
 
 @end
--- ArtExam/AE_draw_info_left.m
+++ ArtExam/AE_draw_info_left.m
@@ -1,8 +1,8 @@
 //
-//  AE—draw_info_left.m
+//  AE_draw_info_left.m
 //  ArtExam
 //
-//  Created by 张亚雄 on 15/8/19.
+//  Created by 张亚雄 on 15/8/20.
 //  Copyright (c) 2015年 张亚雄. All rights reserved.
 //
 
